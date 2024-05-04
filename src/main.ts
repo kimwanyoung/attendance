b3e60e22d53d8fcb6106fcd9fb3e871deefc1a25
kimwanyoung <dhks2869@gmail.com>
2024-05-04 12:29:27 +0900

    feat : https 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import * as fs from "fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync("src/private-key.pem"),
+    cert: fs.readFileSync("src/certificate.pem"),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
