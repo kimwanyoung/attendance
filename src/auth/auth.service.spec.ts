d7c0272257a22241ed030df8fdee501ccfe83ad6
kimwanyoung <dhks2869@gmail.com>
2024-05-19 16:06:24 +0900

    test : user 로그인 테스트

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,44 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
+import { JwtModule } from "@nestjs/jwt";
+import * as bcrypt from "bcrypt";
+import { UserService } from "../user/user.service";
+import { UserModel } from "../user/entity/user.entity";
+import { GenderEnum } from "../user/const/gender.enum";
+import { HASH_ROUND } from "./const/auth.const";
 
 describe("AuthService", () => {
+  let users: UserModel[];
   let service: AuthService;
+  let mockedUserService: Partial<UserService>;
 
   beforeEach(async () => {
+    users = [];
+    mockedUserService = {
+      createUser: async function (
+        user: Pick<
+          UserModel,
+          "email" | "password" | "gender" | "phone" | "name"
+        >,
+      ): Promise<UserModel> {
+        users.push(user as UserModel);
+        return Promise.resolve({ ...user } as UserModel);
+      },
+      async findUserByEmail(email: string): Promise<UserModel> {
+        const user = users.filter((user) => user.email === email);
+        return Promise.resolve(user[0]);
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [JwtModule],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: mockedUserService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +46,22 @@ describe("AuthService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("올바른 이메일, 비밀번호를 입력하면 로그인에 성공한다.", async () => {
+    const hash = await bcrypt.hash("password", HASH_ROUND);
+    await mockedUserService.createUser({
+      email: "dhks2869@gmail.com",
+      password: hash,
+      phone: "01012341234",
+      gender: GenderEnum.MALE,
+      name: "김완영",
+    });
+
+    const user = await service.authenticateWithEmailAndPassword({
+      email: "dhks2869@gmail.com",
+      password: "password",
+    });
+    expect(user.email).toEqual("dhks2869@gmail.com");
   });
 });
