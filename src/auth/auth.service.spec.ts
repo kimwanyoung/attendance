6f5eac00fe51a10155f363c757993e78fa5a01df
kimwanyoung <dhks2869@gmail.com>
2024-05-22 12:19:38 +0900

    test : 존재하지않는 이메일 입력시 예외 테스트

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -31,6 +31,15 @@ describe("AuthService", () => {
       },
     };
 
+    const hash = await bcrypt.hash("password", HASH_ROUND);
+    await mockedUserService.createUser({
+      email: "dhks2869@gmail.com",
+      password: hash,
+      phone: "01012341234",
+      gender: GenderEnum.MALE,
+      name: "김완영",
+    });
+
     const module: TestingModule = await Test.createTestingModule({
       imports: [JwtModule],
       providers: [
@@ -68,19 +77,19 @@ describe("AuthService", () => {
   });
 
   it("올바르지 않은 비밀번호를 입력하면 UnauthorizationException이 발생한다.", async () => {
-    const hash = await bcrypt.hash("password", HASH_ROUND);
-    await mockedUserService.createUser({
-      email: "dhks2869@gmail.com",
-      password: hash,
-      phone: "01012341234",
-      gender: GenderEnum.MALE,
-      name: "김완영",
-    });
-
     await expect(
       service.authenticateWithEmailAndPassword({
         email: "dhks2869@gmail.com",
         password: "aaaa",
+      }),
+    ).rejects.toThrow(UnauthorizedException);
+  });
+
+  it("존재하지 않은 이메일을 입력하면 UnauthorizationException이 발생한다.", async () => {
+    await expect(
+      service.authenticateWithEmailAndPassword({
+        email: "dhks2869@naver.com",
+        password: "password",
       }),
     ).rejects.toThrow(UnauthorizedException);
   });
